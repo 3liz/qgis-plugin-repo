8daa49957c110771eb0c603522a0ea34351ee884
Etienne Trimaille <gustrimaille@yahoo.fr>
2022-06-17 14:43:27 +0200

    Python - Add docstrings and annotations

--- qgis_plugin_repo/merger.py
+++ qgis_plugin_repo/merger.py
@@ -20,8 +20,11 @@ Plugin = namedtuple('Plugin', ['name', 'experimental', 'version'])
 
 class Merger:
 
+    """ To manage a merge between different XML files. """
+
     @classmethod
-    def xml_template(cls):
+    def xml_template(cls) -> str:
+        """ To create an empty XML files. """
         return (
             "<?xml version = '1.0' encoding = 'UTF-8'?>\n"
             "<plugins>\n"
@@ -29,6 +32,7 @@ class Merger:
         )
 
     def __init__(self, destination: Union[Path, None], input_uri: Union[Path, str, None]):
+        """ Constructor. """
         self.destination = destination
         self.input_uri = input_uri
         self.input_parser = None
@@ -36,24 +40,27 @@ class Merger:
         self.output_parser = None
 
     def exists(self) -> bool:
+        """ If the destination files exists. """
         return self.destination.exists()
 
     def init(self) -> None:
+        """ Init the XML files with an empty catalog. """
         print(f"Creating source {self.destination.absolute()}")
         with open(self.destination, 'w', encoding='utf8') as f:
             f.write(self.xml_template())
 
     def xml_input_parser(self) -> ET.Element:
+        """ Returns the XML parser for the input file. """
         if self.input_is_url():
             self.input_parser = ET.fromstring(requests.get(self.input_uri).content)
-            return self.input_parser
-
         else:
             tree = ET.parse(self.input_uri.absolute())
             self.input_parser = tree.getroot()
-            return self.input_parser
+
+        return self.input_parser
 
     def xml_output_parser(self) -> ET.Element:
+        """ Returns the XML parser for the output file. """
         if isinstance(self.destination, str):
             self.destination = Path(self.destination)
 
@@ -65,6 +72,7 @@ class Merger:
         return self.output_parser
 
     def input_is_url(self) -> bool:
+        """ Check if the input is a URL. """
         if isinstance(self.input_uri, Path):
             return False
 
@@ -96,6 +104,7 @@ class Merger:
 
     @staticmethod
     def plugin_element(parser, name: str, experimental: bool) -> ET.Element:
+        """ Return the XML element for a given plugin name and its experimental flag. """
         for plugin in parser:
             if plugin.attrib['name'] != name:
                 continue
@@ -110,7 +119,8 @@ class Merger:
                     return plugin
 
     @staticmethod
-    def diff_plugins(plugins_input, plugins_output):
+    def diff_plugins(plugins_input, plugins_output) -> list:
+        """ List of plugins which are in the input and not in the output. """
         new = []
         for plugin in plugins_input:
             if plugin not in plugins_output:
@@ -119,6 +129,7 @@ class Merger:
         return new
 
     def merge(self):
+        """ Make the merge. """
         if not self.exists():
             raise FileDoesNotExist
 
